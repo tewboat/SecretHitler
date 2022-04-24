c296a96d2ca3afc0ce49d86e9dfe8c1c108d74f7
Ilya <zdravulin@gmail.com>
2022-04-24 23:32:07 +0500

    add player and room classes

---
+++ server/player.js
@@ -0,0 +1,6 @@
+class Player{
+    constructor(name, id){
+        this.name = name;
+        this.id = id;
+    }
+}
--- server/server.js
+++ server/server.js
@@ -5,34 +5,6 @@ let webSocket = WebSocket(uri);
 
 let rooms = new Map();
 
-class Room{
-    id;
-    name;
-    maxPlayers;
-    players = [];
-
-    constructor(roomName, maxPlayers) {
-        this.id = this.generateId()
-        this.name = roomName;
-        this.maxPlayers = maxPlayers;
-    }
-
-    generateId() {
-        return undefined; //todo сделать генерацию уникального идентификатора
-    }
-}
-
-function validateName(){
-    return true; //todo проверить валидность имени
-}
-
-function createRoom(roomName, maxParticipants){
-    if (!validateName(roomName))
-        return; //todo отправлять error_message клиенту
-    let room = new Room(roomName);
-    rooms.set(room.id, room);
-    //todo перенаправить в созданнную комнату
-}
 
 function getRoom(id){
     //todo отправка значения клиенту
