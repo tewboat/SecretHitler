3db848d97a7a0eadd2655f48f872ee9938867966
Ilya Zhdanov <53978939+tewboat@users.noreply.github.com>
2022-06-03 14:42:26 +0500

    Add nickname field

--- server.js
+++ server.js
@@ -7,24 +7,35 @@ const path = require("path");
 
 
 const PORT = 3000;
-
-
 const app = express();
 
 app.use(cookieParser());
 app.use(express.json());
-
+app.use(express.static(path.join(process.cwd(), "views")));
 
 const server = http.createServer(app);
 const io = socketIO(server);
-
-
 const rooms = new Map();
 
+function validateNickname(nickname){
+    const nicknameMaxLength = 15;
+    return 0 < nickname.length < nicknameMaxLength; // TODO сделать проверку уникальности
+}
 
-app.get('/', ((req, res) => {
+app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/views', 'menu.html'));
-}))
+})
+
+app.post('/setName', (req, res) => {
+    let nickname = String(req.body.payload.nickname);
+    if (validateNickname(nickname)){
+        res.cookie('nickname', nickname);
+        res.sendStatus(200);
+    }
+    else {
+        res.sendStatus(422);
+    }
+})
 
 app.get('/enter', (req, res) => {
     let id = req.query.id;
--- views/menu.js
+++ views/menu.js
@@ -1,2 +1,22 @@
-const createRoomButton = document.querySelector('.menu-button-create');
-const findRoomButton = document.querySelector('.menu-button-find');
+const enterButton = document.querySelector('.enter-button');
+const nicknameInput = document.querySelector('.nickname-input');
+
+enterButton.addEventListener('click', async _ => {
+    let response = await fetch('http://localhost:3000/setName', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json;charset=utf-8'
+        },
+        body: JSON.stringify({
+            type: 'info',
+            payload: {
+                nickname: nicknameInput.value
+            }
+        })
+    });
+    if (response.status === 200) {
+        console.log('next step'); // TODO переход к экрану с кнопками
+    } else {
+        // TODO вывод сообщения под полем
+    }
+})
