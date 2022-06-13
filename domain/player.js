a798bfa0305ccea7dc506b8078e967ecfca673e0
artemworld <81902534+artemworld@users.noreply.github.com>
2022-06-13 21:34:40 +0500

    add party generator
    add party generator and fix some bugs

--- domain/player.js
+++ domain/player.js
@@ -1,10 +1,12 @@
 const types = require('./constants');
 
+//TODO do we really need role?
+
 class Player{
     constructor(nickname){
         this.nickname = nickname;
         this.role = types.Role.Player;
-        this.party = types.Party.Liberal;
+        this.party = undefined;
         this.isHitler = false;
     }
 }
