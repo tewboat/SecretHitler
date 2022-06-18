15ec31952366b9d6919486caa3a831ff23b3a61b
Ilya Zhdanov <53978939+tewboat@users.noreply.github.com>
2022-06-18 14:14:04 +0500

    test

--- server.js
+++ server.js
@@ -22,6 +22,7 @@ app.use(cookieParser());
 app.use(express.json());
 app.use(express.static(path.join(process.cwd(), "views")));
 app.use(redirectToLoginPage);
+app.enable('trust proxy')
 
 const server = https.createServer(
     {
